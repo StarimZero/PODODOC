69c5383c152c530381c35f9fdc54f49b1ee4ddd5
Seoptrike <dlstjq977@naver.com>
2024-09-05 20:32:47 +0900

    like

--- FRONTEND/app/build.gradle.kts
+++ FRONTEND/app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
